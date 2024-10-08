0f7957117a0bd059400a69486750424cfa1ba8bf
Noam Favier <95364866+NoamFav@users.noreply.github.com>
2024-10-08 22:52:15 +0200

    auto-commit

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,16 +4,10 @@ module com.example.umproject {
     requires org.jetbrains.annotations;
     requires transitive javafx.graphics;
 
-    opens com.umproject to javafx.fxml;
     exports com.umproject;
     exports com.umproject.Graphs;
-    opens com.umproject.Graphs to javafx.fxml;
     exports com.umproject.Cards;
-    opens com.umproject.Cards to javafx.fxml;
     exports com.umproject.MainPage;
-    opens com.umproject.MainPage to javafx.fxml;
     exports com.umproject.AI;
-    opens com.umproject.AI to javafx.fxml;
     exports com.umproject.Utils;
-    opens com.umproject.Utils to javafx.fxml;
 }
